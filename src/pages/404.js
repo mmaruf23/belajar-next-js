4e2a6a75094b9042ee6a55bf67693eb81bfb727b
mmaruf23 <maruuf82@gmail.com>
2025-01-31 13:43:51 +0700

    swr

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,33 +1,37 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
+
+
 export const NotFoundPage = () => {
+  const router = useRouter();
   return (
     <div className="bg-indigo-900 relative overflow-hidden h-screen">
-       
-      <img
+      <Image
         src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9"
         className="absolute h-full w-full object-cover"
         alt="404"
+        width={10000}
+        height={10000}
       />
-       
-      <div className="inset-0 bg-black opacity-25 absolute">  </div>
-       
+
+      <div className="inset-0 bg-black opacity-25 absolute"> </div>
+
       <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
-           
         <div className="w-full font-mono flex flex-col items-center relative z-10">
-               
           <h1 className="font-extrabold text-5xl text-center text-white leading-tight mt-4">
-             You are all alone here     
+            You are all alone here
           </h1>
-               
+          <span onClick={() => router.back()} className="font-extrabold text-5xl text-center text-white leading-tight mt-4 uppercase p-4 bg-black rounded-xl hover:bg-gray-700">
+            back
+          </span>
+
           <p className="font-extrabold text-8xl my-44 text-white animate-bounce">
-                    404 
-                
+            404
           </p>
-             
         </div>
-         
       </div>
     </div>
   );
--- src/pages/products/[id]/index.js
+++ src/pages/products/[id]/index.js
@@ -1,20 +1,44 @@
 import { formatCurrency } from '@/helpers/util/formatCurrency';
 import { getProductById } from '@/services/products';
+import axios from 'axios';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 import React from 'react';
+import useSWR from 'swr';
 
 const ProductDetailPage = ({ detailProducts }) => {
-  console.log(detailProducts);
+  // console.log(detailProducts);
+
+  /**
+   * useSWR (Stale Whale Revalidate) : hooks third party dari tim vercel untuk fetching data , catching dna revalidate disisi client,
+   * rumus : const { data, error, isLoading, isValidating } = useSWR(key(endpoint), dataFetcher)
+   * swr punya beberapa properti
+   * data : data yang diambil dari API
+   * error : error handling saat ambil data
+   * isLoading : status loading
+   * isValidating : status validasi ulang data (perbarui data)
+   */
+  const api = process.env.NEXT_PUBLIC_API;
+  const { data } = useSWR(
+    `${api}/products/${detailProducts?.id}`,
+    async () => {
+      const res = await axios.get(`${api}/products/${detailProducts?.id}`);
+      return res.data;
+    },
+    {
+      initialData: detailProducts,
+    }
+  );
 
   return (
     <>
       <div className="flex flex-col px-5 py-5 bg-gradient-to-b from-black to-blue-900 min-h-screen">
         <h1 className="text-4xl font-bold text-white">Detail Products</h1>
         <div className="p-4 mt-5 rounded-2xl bg-white bg-opacity-20 max-w-xl">
-          <Image className='p-4 rounded-lg w-full aspect-video object-contain' src={detailProducts?.image} width={300} height={300} alt='Image' />
-          <h2 className='text-2xl font-bold text-white'>{detailProducts?.title}</h2>
-          <p className="text-white font-semibold mt-5">{detailProducts.description}</p>
+          <Image className='p-4 rounded-lg w-full aspect-video object-contain' src={data?.image} width={300} height={300} alt='Image' />
+
+          <h2 className='text-2xl font-bold text-white'>{data?.title}</h2>
+          <p className="text-white font-semibold mt-5">{data?.description}</p>
           <p className="text-white text-xl font-bold mt-5">{formatCurrency(detailProducts.price * 5, "en-US", "USD")}</p>
         </div>
       </div>
@@ -26,15 +50,15 @@ export async function getServerSideProps(context) {
   const { query } = context;
   console.log(query);
   console.log(query.id);
-  
+
 
   try {
     const detailProducts = await getProductById(id);
 
-    if (!detailProducts){
+    if (!detailProducts) {
       return {
         notFound: true
-      }
+      };
     }
 
 
